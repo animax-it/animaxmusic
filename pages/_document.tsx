c9c8d8fe908a66b82051f74596e14670b8da1a38
SharanyaMBhat <sharanya.m@target.com>
2023-11-21 21:15:24 +0530

    Updated website

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   const AnyComponent = Head as any;
+  const AnyComponent2 = NextScript as any;
+
   return (
     <Html>
       <AnyComponent>
@@ -12,7 +14,7 @@ export default function Document() {
       </AnyComponent>
       <body>
         <Main />
-        <NextScript />
+        <AnyComponent2 />
       </body>
     </Html>
   );
