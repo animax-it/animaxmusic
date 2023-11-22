d2ce7151752125fcb624a97b1569de40b503b200
SharanyaMBhat <sharanya.m@target.com>
2023-11-23 03:10:14 +0530

    final changes

--- pages/my-work.tsx
+++ pages/my-work.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
-import { Stack, Typography, Grid, IconButton} from '@mui/material';
+import { Stack, Typography, Grid, IconButton, CircularProgress } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 import Layout from '../components/Layout';
@@ -8,6 +8,13 @@ import Layout from '../components/Layout';
 const MyWork: NextPage = () => {
   const { t } = useTranslation('work');
   const workLinks: string[] = t('body', { returnObjects: true });
+  const [loading, setLoading] = useState<boolean[]>(new Array(workLinks.length).fill(true));
+
+  const handleLoad = (index: number) => {
+    const updatedLoading = [...loading];
+    updatedLoading[index] = false;
+    setLoading(updatedLoading);
+  };
 
   return (
     <Layout pageTitle={t('page-title')} centered>
@@ -19,15 +26,23 @@ const MyWork: NextPage = () => {
         <Grid container spacing={2}>
           {workLinks.map((videoUrl, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
-              <IconButton>
-                <iframe
-                  width="100%"
-                  height="100%"
-                  src={videoUrl}
-                  frameBorder="0"
-                  allowFullScreen
-                />
-              </IconButton>
+              <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                {loading[index] && (
+                  <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                    <CircularProgress />
+                  </div>
+                )}
+                <IconButton>
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    src={videoUrl}
+                    frameBorder="0"
+                    allowFullScreen
+                    onLoad={() => handleLoad(index)}
+                  />
+                </IconButton>
+              </div>
             </Grid>
           ))}
         </Grid>
